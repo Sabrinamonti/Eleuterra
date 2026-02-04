5089d8c657ed3814fe4269f358b6116555059fad
esturbio <estebanjc2010@gmail.com>
2026-02-03 21:19:44 -0400

    test

---
+++ lib/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _LoginPagState extends StatefulWidget {
+  const _LoginPagState({super.key});
+
+  @override
+  State<_LoginPagState> createState() => __LoginPagStateState();
+}
+
+class __LoginPagStateState extends State<_LoginPagState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
